05d4e0e5fc8e6fb917886e0935f443d25519d34a
Joshua Hew <66423860+J2hwz@users.noreply.github.com>
2025-02-10 15:48:36 +0000

    Update 2025-02-10
    Finished experiment

--- data_includes/main.js
+++ data_includes/main.js
@@ -10,16 +10,18 @@ PennController.DebugOff();
 
 // this _should_ increment the counter immediately and ensure that there is a good mix of groups
 SetCounter("inc", 1);
-var counterOverride = 2; // group 3 <- Change this to change starting group
+// var counterOverride = 2; // group 3 <- Change this to change starting group
 
 
 Header(
+// void
 )
+.log("sonaID", GetURLParameter("id"))
 
 
-.log( "PROLIFIC_PID" , GetURLParameter("PROLIFIC_PID") ); // Add the ID to all trials' results lines
+// .log( "PROLIFIC_PID" , GetURLParameter("PROLIFIC_PID") ); // Add the ID to all trials' results lines
 
-// The following is the definition of dashed; customized self-paced reading controller
+// The following is the definition of dashed; customized self-paced reading controller (ORIGINAL CODE)
 
 dashed = (name,sentence) => [
     newText(name,"").css({display:'flex','flex-direction':'row','flex-wrap':'wrap','line-height':'2em','max-width':'100%'}).print()
@@ -28,7 +30,7 @@ dashed = (name,sentence) => [
             newText("").css("width","100vw").print(getText(name))
             :
             newText(name+'-'+i, w.replace(/([^\s])/g,'_'))
-                .css({margin:"0em 0.2em",'font-family':'monospace',"font-size":"12pt"})
+                .css({margin:"0em 0.2em",'font-family':'monospace, monospace',"font-size":"18px"}) // have to use a monospace font for self-paced reading to work
                 .print(getText(name))
     ))
     ,
@@ -47,51 +49,85 @@ dashed = (name,sentence) => [
 // Not sure if you need to remove it when you set the group manually (try it out to be sure)
 // If you don't include "increase" explicitly, I think what happens is that the increment happens
 // right at the end (then you will get loads in the first group if you are doing it automatically)
-Sequence("increase", "demographics", "consent", "demo","demo2","continuation-message",rshuffle("filler","exp"),SendResults(),"final")
+Sequence(
+    "increase", 
+    "consent", 
+    "demographics", 
+    "demo",
+    "demo2",
+    "continuation-message",
+    rshuffle("filler","exp"),
+    SendResults(),
+    "final")
 
-
-newTrial("demographics", 
-    newText("demographics-text", "Please provide the following demographic information.")
-        .print(),
-    newText("gender-text", "Please indicate your gender")
-        .print(),
-    newDropDown("gender","")
-        .log()
-        .add("Male", "Female", "Non-binary / other", "Prefer not to say")
-        .print(),
-    newText("age-text", "Please enter your age")
-        .print(),
-    newTextInput("age","")
-        .print(),
-    newText("native-text", "Are you a native British English speaker?")
-        .print(),
-    newDropDown("native","")
-        .log()
-        .add("Yes, I am a native British English speaker.", 
-            "No, I am a native English speaker but not from Britain (specify if you are comfortable to do so)", 
-            "No, I am not a native English speaker (specify if you are comfortable to do so)")
-        .print(),
-    
-    newButton("send", "Proceed")
-        .print()
-        .wait()
-        // .wait(getTextInput("age").test.text(/^(0|[1-9][0-9]*)$/))
-        // .failure(newText("integer", "Please input an integer")
-)
-
-
+// Consent form code
 newTrial("consent",
     newHtml("consent_form", "consent.html")
         .cssContainer({"width":"720px"})
         .checkboxWarning("You must consent before continuing.")
         .print()
     ,
-    newButton("continue", "Click to agree with 1-3 above")
+    newButton("continue", "Click to agree with 1 - 3 above")
         .center()
         .print()
         .wait(getHtml("consent_form").test.complete()
         .failure(getHtml("consent_form").warn())
         )
+)
+
+// Demographics slide
+newTrial("demographics", 
+    newText("demographics-text", "<h2>Please provide the following demographic information.</h2>")
+        .print(),
+        
+    newText("gender_text", "Please indicate your gender")
+        .print(),
+    newScale("gender", "Male", "Female", "Non-binary / other", "Prefer not to say")
+        .log()
+        .button()
+        .print()
+        .wait(),
+        
+    newText("age_text", "Please enter your age (Press the 'Enter' key after typing)")
+        .print(),
+    newTextInput("age","")
+        .log()
+        .length(3)
+        .print()
+        .wait(getTextInput("age").test.text(/^(0|[1-9][0-9]*)$/)),
+        
+    newText("english_text", "Are you a native British English speaker?")
+        .print(),
+    newScale("english",
+            "Yes, I am a native British English speaker", 
+            "No, I am a native English speaker but not from Britain", 
+            "No, I am not a native English speaker")
+        .log()
+        .button()
+        .print()
+        .wait(),
+        
+    newText("english_age_text", "Did you learn English before the age of 5?")
+        .print(),
+    newScale("english_age", "Yes", "No", "Prefer not to say")
+        .log()
+        .button()
+        .print()
+        .wait(),
+        
+    newText("difficulty_reading_text", "Do you have any known difficulty with reading English, such as autism, dyslexia, or schizophrenia?")
+        .print(),
+    newScale("difficulty_reading", "Yes", "No", "Prefer not to say")
+        .log()
+        .button()
+        .print()
+        .wait(),
+        
+    newButton("send", "Proceed")
+        .print()
+        .wait()
+        // .wait(getTextInput("age").test.text(/^(0|[1-9][0-9]*)$/))
+        // .failure(newText("integer", "Please input an integer")
 )
 
 
@@ -170,10 +206,10 @@ newTrial("demo",
     getText("instruction-6")
         .remove()
     ,  
-     getText("instruction-7")
+    getText("instruction-7")
         .remove()
     ,  
-         getText("instruction-8")
+    getText("instruction-8")
         .remove()
     ,  
     newTimer("break", 1000)
@@ -181,40 +217,41 @@ newTrial("demo",
         .wait()
     ,    
     newText("firstdisplay", "Keep pressing the SPACEBAR")
-    .color("blue")
-    .print()
-,
-newKey("spacebar", " ")
-    .log()
-    .wait()
-,
-    newController("DashedSentence", {s : "You have just begun reading the sentence you have just finished reading."})
-        .center()
+        .color("blue")
         .print()
+    ,
+    newKey("spacebar", " ")
+        .log()
         .wait()
-        .remove()
+    ,
+    // newText("test1_text", "Please read carefully")
+    //     .print(),
+    newTimer("break", 400)
+        .start()
+        .wait(),
+    dashed("test1", "Liam and Ava went to a yoga class.*Ava struggled with some of the poses.*Liam suggested that*with more*practice,*it would get*easier."),
+    getText("test1").remove()
+    // newController("DashedSentence", {s : "You have just begun reading the sentence you have just finished reading."})
+    //     .center()
+    //     .print()
+    //     .wait()
+    //     .remove()
     )
     
-    newTrial("demo2",
+newTrial("demo2",
 
-    newTimer("break", 1000)
+    newTimer("break", 400)
         .start()
         .wait()
     ,
-    // dashed("example","This is*my dog*and it likes*to chase*rabbits." ),
-    newController("DashedSentence", {s : "My dog likes chasing rabbits and my cat hates the dog."})
-//    dashed("example","This is*my dog*and it likes*to chase*rabbits." )
-        .center()
-        .print()
-        .wait()
-        .remove()
-    ,
+    dashed("test2", 'Sophia and Daniel joined a trivia night at a pub.*Their team lost by one point because Daniel second-guessed an answer.*Sophia told him:*"You should*trust*your instincts*next time."'),
+    getText("test2").remove(),
     newText("questionhint", "Press either 1 or 2")
-    .center()
-    .color("blue")
-    .print()
+        .center()
+        .color("blue")
+        .print()
     ,
-    newController("Question", {q : "Does my dog like chasing rabbits?", as: ["Yes","No"], hasCorrect:0,randomOrder:false})
+    newController("Question", {q : "Were Sophia and Daniel at the pub?", as: ["Yes","No"], hasCorrect:0,randomOrder:false})
         .center()
         .print()
         .wait()
@@ -232,18 +269,32 @@ newKey("spacebar", " ")
         .wait()
 )
 
+// newTrial( "final" ,
+//      newText("<p>Thank you for your participation!</p>")
+//             .center()
+//           .print()
+//     ,
+//     newText("<p><a href='https://edinburgh.eu.qualtrics.com/jfe/form/SV_ehbo4ZELmAoAhds?PROLIFIC_PID="+GetURLParameter("PROLIFIC_PID")+"'>Please click here to continue the experiment</a></p>")
+//         .center()
+//         .print()
+//     ,
+//     newButton("void")
+//     .wait()
+//     )
+
+// SONA version
 newTrial( "final" ,
      newText("<p>Thank you for your participation!</p>")
-            .center()
-           .print()
-    ,
-    newText("<p><a href='https://edinburgh.eu.qualtrics.com/jfe/form/SV_ehbo4ZELmAoAhds?PROLIFIC_PID="+GetURLParameter("PROLIFIC_PID")+"'>Please click here to continue the experiment</a></p>")
-        .center()
-        .print()
-    ,
-    newButton("void")
-    .wait()
-    )
+          .center()
+          .print()
+     ,
+     newText("<p><a href='https://universityofedinburgh-ppls.sona-systems.com/webstudy_credit.aspx?experiment_id=981&credit_token=c0143c3b451048318b71b248f382ecf7&survey_code="+GetURLParameter("id")+"' target='_blank'>Click here to confirm your participation on SONA.</a></p> <p>This is a necessary step in order for you to receive participation credit!</p>")
+          .center()
+          .print()
+     ,
+     newButton("void")
+          .wait()
+)
 
 
 
